834cc6dc9b558bb934d8325619249f4a6c812e61
Sebastien Gaumin <sgaumin@gmail.com>
2019-11-15 15:30:05 +0100

    [Feature] Added Color Extensions script.

---
+++ Extensions/ColorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public static class ColorExtensions
+{
+	/// <summary>
+	/// Create a new color from the given color and a new value for its alpha.
+	/// </summary>
+	/// <param name="c">The source color</param>
+	/// <param name="a">The new alpha value</param>
+	/// <returns>A new color {c.rgb, a}</returns>
+	public static Color WithAlpha(this Color c, float a)
+	{
+		c.a = a;
+		return c;
+	}
+
+	/// <summary>
+	/// Create a new color from the given color with its alpha multiplied.
+	/// </summary>
+	/// <param name="c">The source color</param>
+	/// <param name="a">A factor to multiply the alpha value with</param>
+	/// <returns>A new color {c.rgb, a}</returns>
+	public static Color WithAlphaMultiplied(this Color c, float a)
+	{
+		c.a *= a;
+		return c;
+	}
+}
