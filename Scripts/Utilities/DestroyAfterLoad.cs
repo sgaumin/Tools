458cbc04159d2a8bb55b7ec013a209e793f4a8a0
Sebastien Gaumin <sgaumin@gmail.com>
2019-11-17 09:05:12 +0100

    [Refacto] DestroyAfterLoad script refactored.

--- Scripts/Utilities/DestroyAfterLoad.cs
+++ Scripts/Utilities/DestroyAfterLoad.cs
@@ -2,5 +2,9 @@
 
 public class DestroyAfterLoad : MonoBehaviour
 {
+	[SerializeField] private float durationBeforeDestroy;
+
+	private void Start() => Initialize(durationBeforeDestroy);
+
 	public void Initialize(float duration) => Destroy(gameObject, duration);
 }
