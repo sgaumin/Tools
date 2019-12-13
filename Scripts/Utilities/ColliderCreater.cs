9df6eeaa450ff63e75cc879f07b5a564631191a9
Sebastien Gaumin <sgaumin@gmail.com>
2019-12-13 15:29:24 +0100

    [Feature] Collider Creater prefab added.

---
+++ Scripts/Utilities/ColliderCreater.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(EdgeCollider2D))]
+public class ColliderCreater : MonoBehaviour
+{
+	[SerializeField] private Transform pointsHolder;
+
+	private Vector2? firstPoint;
+
+	protected void Start()
+	{
+		List<Vector2> positions = new List<Vector2>();
+		firstPoint = null;
+
+		foreach (Transform point in pointsHolder)
+		{
+			if (point != transform)
+			{
+				if (firstPoint == null)
+				{
+					firstPoint = point.position;
+				}
+
+				positions.Add(point.position);
+			}
+		}
+
+		positions.Add((Vector2)firstPoint);
+
+		GetComponent<EdgeCollider2D>().points = positions.ToArray();
+	}
+}
