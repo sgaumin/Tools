63d3f3d9bffed88407921c73d62e53e5c0da5296
Sebastien Gaumin <sgaumin@gmail.com>
2020-04-09 20:25:01 +0900

    [Refacto] FadScreen reworked.

--- Scripts/Utils/FadScreen.cs
+++ Scripts/Utils/FadScreen.cs
@@ -5,42 +5,44 @@ using UnityEngine.UI;
 
 namespace Tools.Utils
 {
-	public class FadScreen : MonoBehaviour
-	{
-		public static FadScreen Instance { get; private set; }
+    public class FadScreen : MonoBehaviour
+    {
+        public static FadScreen Instance { get; private set; }
 
-		private Image image;
+        private Color defaultFadColorTarget = Color.black;
+        private Ease defaultEase = Ease.Linear;
+        private Image image;
 
-		private void Awake()
-		{
-			Instance = this;
-			image = GetComponentInChildren<Image>();
-		}
+        private void Awake()
+        {
+            Instance = this;
+            image = GetComponentInChildren<Image>();
+        }
 
-		public void FadOut(Color colorTarget, float fadDuration = 1f)
-		{
-			image.color = new Color(colorTarget.r, colorTarget.g, colorTarget.b, 0f);
-			image.DOFade(1f, fadDuration).Play();
-		}
+        public void FadOut(Color? colorTarget = null, float fadDuration = 1f, Ease? ease = null)
+        {
+            image.color = colorTarget != null ? new Color((float)colorTarget?.r, (float)colorTarget?.g, (float)colorTarget?.b, 0f) : (Color)colorTarget;
+            image.DOFade(1f, fadDuration).SetEase(ease ?? defaultEase).Play();
+        }
 
-		public IEnumerator FadOutCore(Color colorTarget, float fadDuration = 1f)
-		{
-			image.color = new Color(colorTarget.r, colorTarget.g, colorTarget.b, 0f);
-			Tweener fad = image.DOFade(1f, fadDuration).Play();
-			yield return fad.WaitForCompletion();
-		}
+        public IEnumerator FadOutCore(Color? colorTarget = null, float fadDuration = 1f, Ease? ease = null)
+        {
+            image.color = colorTarget != null ? new Color((float)colorTarget?.r, (float)colorTarget?.g, (float)colorTarget?.b, 0f) : (Color)colorTarget;
+            Tweener fad = image.DOFade(1f, fadDuration).SetEase(ease ?? defaultEase).Play();
+            yield return fad.WaitForCompletion();
+        }
 
-		public void FadIn(Color colorTarget, float fadDuration = 1f)
-		{
-			image.color = colorTarget;
-			image.DOFade(0f, fadDuration).Play();
-		}
+        public void FadIn(Color? colorTarget = null, float fadDuration = 1f, Ease? ease = null)
+        {
+            image.color = colorTarget ?? defaultFadColorTarget;
+            image.DOFade(0f, fadDuration).SetEase(ease ?? defaultEase).Play();
+        }
 
-		public IEnumerator FadInCore(Color colorTarget, float fadDuration = 1f)
-		{
-			image.color = colorTarget;
-			Tweener fad = image.DOFade(0f, fadDuration).Play();
-			yield return fad.WaitForCompletion();
-		}
-	}
+        public IEnumerator FadInCore(Color? colorTarget = null, float fadDuration = 1f, Ease? ease = null)
+        {
+            image.color = colorTarget ?? defaultFadColorTarget;
+            Tweener fad = image.DOFade(0f, fadDuration).SetEase(ease ?? defaultEase).Play();
+            yield return fad.WaitForCompletion();
+        }
+    }
 }
