8b1aeb5ed378ed3b2491f75567cea92f278ae27c
Sebastien Gaumin <sgaumin@gmail.com>
2020-04-11 13:58:50 +0900

    [Fix] FadOut default color fixed.

--- Scripts/Utils/FadScreen.cs
+++ Scripts/Utils/FadScreen.cs
@@ -9,7 +9,8 @@ namespace Tools.Utils
     {
         public static FadScreen Instance { get; private set; }
 
-        private Color defaultFadColorTarget = Color.black;
+        private Color defaultFadInColorTarget = Color.black;
+        private Color defaultFadOutColorTarget = Color.black.WithAlpha(0);
         private Ease defaultEase = Ease.Linear;
         private Image image;
 
@@ -21,26 +22,26 @@ namespace Tools.Utils
 
         public void FadOut(Color? colorTarget = null, float fadDuration = 1f, Ease? ease = null)
         {
-            image.color = colorTarget != null ? new Color((float)colorTarget?.r, (float)colorTarget?.g, (float)colorTarget?.b, 0f) : defaultFadColorTarget;
+            image.color = colorTarget != null ? new Color((float)colorTarget?.r, (float)colorTarget?.g, (float)colorTarget?.b, 0f) : defaultFadOutColorTarget;
             image.DOFade(1f, fadDuration).SetEase(ease ?? defaultEase).Play();
         }
 
         public IEnumerator FadOutCore(Color? colorTarget = null, float fadDuration = 1f, Ease? ease = null)
         {
-            image.color = colorTarget != null ? new Color((float)colorTarget?.r, (float)colorTarget?.g, (float)colorTarget?.b, 0f) : defaultFadColorTarget;
+            image.color = colorTarget != null ? new Color((float)colorTarget?.r, (float)colorTarget?.g, (float)colorTarget?.b, 0f) : defaultFadOutColorTarget;
             Tweener fad = image.DOFade(1f, fadDuration).SetEase(ease ?? defaultEase).Play();
             yield return fad.WaitForCompletion();
         }
 
         public void FadIn(Color? colorTarget = null, float fadDuration = 1f, Ease? ease = null)
         {
-            image.color = colorTarget ?? defaultFadColorTarget;
+            image.color = colorTarget ?? defaultFadInColorTarget;
             image.DOFade(0f, fadDuration).SetEase(ease ?? defaultEase).Play();
         }
 
         public IEnumerator FadInCore(Color? colorTarget = null, float fadDuration = 1f, Ease? ease = null)
         {
-            image.color = colorTarget ?? defaultFadColorTarget;
+            image.color = colorTarget ?? defaultFadInColorTarget;
             Tweener fad = image.DOFade(0f, fadDuration).SetEase(ease ?? defaultEase).Play();
             yield return fad.WaitForCompletion();
         }
