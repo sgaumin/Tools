f5e53379ba5c9481455b74332638f45a31812f6f
Sebastien Gaumin <sgaumin@gmail.com>
2020-11-01 00:50:35 +0900

    [Feature] Vector2 rotate method add

--- Extensions/Vector2Extensions.cs
+++ Extensions/Vector2Extensions.cs
@@ -13,4 +13,16 @@ public static class Vector2Extensions
 	public static Vector2 timesX(this Vector2 vector, float timesX) => new Vector2(vector.x * timesX, vector.y);
 
 	public static Vector2 timesY(this Vector2 vector, float timesY) => new Vector2(vector.x, vector.y * timesY);
+
+	public static Vector2 Rotate(this Vector2 v, float degrees)
+	{
+		float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
+		float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
+
+		float tx = v.x;
+		float ty = v.y;
+		v.x = (cos * tx) - (sin * ty);
+		v.y = (sin * tx) + (cos * ty);
+		return v;
+	}
 }
