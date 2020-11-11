bca155c45d0b248fb9f2359777dc1944726b7f0b
Sebastien Gaumin <sgaumin@gmail.com>
2020-11-11 21:33:13 +0900

    [Feature] GameObject extensions creation

---
+++ Extensions/GameObjectExtensions.cs
@@ -0,0 +1,56 @@
+﻿using DG.Tweening;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class GameObjectExtensions
+{
+	public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
+	{
+		return gameObject.GetComponent<T>() ?? gameObject.AddComponent<T>();
+	}
+
+	public static List<T> GetAllComponents<T>(this GameObject gameObject) where T : Component
+	{
+		List<T> allColliders = new List<T>();
+
+		gameObject.GetComponents<T>()?.ForEach(x => allColliders.Add(x));
+		gameObject.GetComponentsInChildren<T>()?.ForEach(x => allColliders.Add(x));
+
+		return allColliders;
+	}
+
+	public static void EnableColliders(this GameObject gameObject)
+	{
+		gameObject.GetAllComponents<Collider>()?.ForEach(x => x.enabled = true);
+		gameObject.GetAllComponents<Collider2D>()?.ForEach(x => x.enabled = true);
+	}
+
+	public static void DisableColliders(this GameObject gameObject)
+	{
+		gameObject.GetAllComponents<Collider>()?.ForEach(x => x.enabled = false);
+		gameObject.GetAllComponents<Collider2D>()?.ForEach(x => x.enabled = false);
+	}
+
+	public static void FadIn(this GameObject gameObject, float duration = 1f)
+	{
+		gameObject.GetAllComponents<SpriteRenderer>()?.ForEach(x => x.color = x.color.WithAlpha(0f));
+		gameObject.GetAllComponents<Image>()?.ForEach(x => x.color = x.color.WithAlpha(0f));
+		gameObject.GetAllComponents<TextMeshPro>()?.ForEach(x => x.color = x.color.WithAlpha(0f));
+		gameObject.GetAllComponents<TextMeshProUGUI>()?.ForEach(x => x.color = x.color.WithAlpha(0f));
+
+		gameObject.GetAllComponents<SpriteRenderer>()?.ForEach(x => x.DOFade(1f, duration));
+		gameObject.GetAllComponents<Image>()?.ForEach(x => x.DOFade(1f, duration));
+		gameObject.GetAllComponents<TextMeshPro>()?.ForEach(x => x.DOFade(1f, duration));
+		gameObject.GetAllComponents<TextMeshProUGUI>()?.ForEach(x => x.DOFade(1f, duration));
+	}
+
+	public static void FadOut(this GameObject gameObject, float duration = 1f)
+	{
+		gameObject.GetAllComponents<SpriteRenderer>()?.ForEach(x => x.DOFade(0f, duration));
+		gameObject.GetAllComponents<Image>()?.ForEach(x => x.DOFade(0f, duration));
+		gameObject.GetAllComponents<TextMeshPro>()?.ForEach(x => x.DOFade(0f, duration));
+		gameObject.GetAllComponents<TextMeshProUGUI>()?.ForEach(x => x.DOFade(0f, duration));
+	}
+}
