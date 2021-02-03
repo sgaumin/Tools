b0a4780da6249021ccab29bdcf9de1c7046d872e
Sebastien Gm <sgaumin@gmail.com>
2021-02-03 20:25:09 +0900

    [Feature] Transform context menu method creation

---
+++ Scripts/Editor/TransformContextMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Tools.Utils
+{
+	public class TransformContextMenu
+	{
+		[MenuItem("CONTEXT/Transform/Apply Transform to Children and Reset")]
+		static void ApplyTransformToChildrenAndReset()
+		{
+			foreach (Transform child in Selection.activeTransform)
+			{
+				if (child == Selection.activeTransform)
+					continue;
+
+				child.position += Selection.activeTransform.position;
+				child.rotation *= Selection.activeTransform.rotation;
+				child.localScale = Vector3.Scale(Selection.activeTransform.localScale, child.localScale);
+			}
+
+			Selection.activeTransform.position = Vector3.zero;
+			Selection.activeTransform.rotation = Quaternion.identity;
+			Selection.activeTransform.localScale = Vector3.one;
+		}
+	}
+}
